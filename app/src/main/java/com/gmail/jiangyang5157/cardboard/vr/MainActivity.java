bb326021c3e3532f009b0952fe5fe6e460382b79
Yang Jiang <jiangyang5157@gmail.com>
2016-03-20 18:06:59 +1300

    add libs/res/raw/img

--- app/src/main/java/com/gmail/jiangyang5157/cardboard/vr/MainActivity.java
+++ app/src/main/java/com/gmail/jiangyang5157/cardboard/vr/MainActivity.java
@@ -1,13 +1,67 @@
 package com.gmail.jiangyang5157.cardboard.vr;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Context;
+import android.os.Vibrator;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.gmail.jiangyang5157.cardboard.ui.CardboardOverlayView;
+import com.google.vrtoolkit.cardboard.CardboardActivity;
+import com.google.vrtoolkit.cardboard.CardboardView;
+import com.google.vrtoolkit.cardboard.Eye;
+import com.google.vrtoolkit.cardboard.HeadTransform;
+import com.google.vrtoolkit.cardboard.Viewport;
+
+import javax.microedition.khronos.egl.EGLConfig;
+
+public class MainActivity extends CardboardActivity implements CardboardView.StereoRenderer {
+    private static final String TAG = "MainActivity";
+
+    private CardboardOverlayView overlayView;
+
+    private Vibrator vibrator;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        CardboardView cardboardView = (CardboardView) findViewById(R.id.cardboard_view);
+        cardboardView.setRestoreGLStateEnabled(false);
+        cardboardView.setRenderer(this);
+        setCardboardView(cardboardView);
+
+        overlayView = (CardboardOverlayView) findViewById(R.id.cardboard_overlay_view);
+
+        vibrator = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
+    }
+
+    @Override
+    public void onNewFrame(HeadTransform headTransform) {
+
+    }
+
+    @Override
+    public void onDrawEye(Eye eye) {
+
+    }
+
+    @Override
+    public void onFinishFrame(Viewport viewport) {
+
+    }
+
+    @Override
+    public void onSurfaceChanged(int i, int i1) {
+
+    }
+
+    @Override
+    public void onSurfaceCreated(EGLConfig eglConfig) {
+
+    }
+
+    @Override
+    public void onRendererShutdown() {
+
     }
 }
