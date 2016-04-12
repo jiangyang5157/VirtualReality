d98924196229b18be956a1c2927a2e67b6d312f7
Yang Jiang <jiangyang5157@gmail.com>
2016-04-13 01:01:19 +1200

    NA

--- app/src/main/java/com/gmail/jiangyang5157/cardboard/scene/Geometry.java
+++ app/src/main/java/com/gmail/jiangyang5157/cardboard/scene/Geometry.java
@@ -2,6 +2,9 @@ package com.gmail.jiangyang5157.cardboard.scene;
 
 /**
  * Created by Yang on 4/9/2016.
+ *
+ * angle [0, 180] = 180 / PI * radian
+ * radian [0, PI] = PI / 180 * angle
  */
 interface Geometry {
     //golden ratio: https://en.wikipedia.org/wiki/Regular_icosahedron
@@ -9,4 +12,5 @@ interface Geometry {
 
     float PI = (float) Math.PI;
     float PIx2 = PI * 2.0f;
+
 }
