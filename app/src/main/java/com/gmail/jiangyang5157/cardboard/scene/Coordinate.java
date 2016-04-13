455c1512f7c814ab6df10f5e4293e42bd0df82e4
Yang Jiang <jiangyang5157@gmail.com>
2016-04-13 21:01:05 +1200

    add alt

--- app/src/main/java/com/gmail/jiangyang5157/cardboard/scene/Coordinate.java
+++ app/src/main/java/com/gmail/jiangyang5157/cardboard/scene/Coordinate.java
@@ -7,17 +7,19 @@ public class Coordinate {
 
     private float longitude;
     private float latitude;
+    private float altitude;
 
-    public Coordinate(float longitude, float latitude) {
-        set(longitude, latitude);
+    public Coordinate(float longitude, float latitude, float altitude) {
+        set(longitude, latitude, altitude);
     }
 
-    public void set(float longitude, float latitude) {
+    public void set(float longitude, float latitude, float altitude) {
         if (longitude < -180 || longitude > 180 || latitude < -90 || latitude > 90) {
             throw new IllegalArgumentException("longitude [-180, 180], latitude [-90, 90]");
         }
 
         this.longitude = Math.round(longitude * PRECISION_FACTOR) / PRECISION_FACTOR;
         this.latitude = Math.round(latitude * PRECISION_FACTOR) / PRECISION_FACTOR;
+        this.altitude = altitude;
     }
 }
