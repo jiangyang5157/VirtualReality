0aa9c6374cd2be9324a5b835a66eee72ec884de0
Yang Jiang <jiangyang5157@gmail.com>
2016-04-18 22:27:47 +1200

    ecef match my coord sys, but not flipped texture

--- app/src/main/java/com/gmail/jiangyang5157/cardboard/scene/Coordinate.java
+++ app/src/main/java/com/gmail/jiangyang5157/cardboard/scene/Coordinate.java
@@ -4,20 +4,18 @@ import android.util.Log;
 
 public class Coordinate {
 
-    private static final double WGS84_A = 6378137.0;
-    private static final double WGS84_E = 8.1819190842622e-2;
+    public static final double WGS84_SEMI_MAJOR_AXIS = 6378137.0;
+    public static final double WGS84_FLATTENING = 1.0 / 298.257222101;
+    public static final double WGS84_ECCENTRICITY = Math.sqrt(1 - Math.pow((1 - WGS84_FLATTENING), 2));
 
     double[] lla;
     double[] ecef;
-    double[] enu;
 
     public Coordinate(double latitude, double longitude, double altitude, double a, double e) {
         setLla(latitude, longitude, altitude);
         Log.i("####", "lla: " + String.format("%.5f", lla[0]) + ", " + String.format("%.5f", lla[1]) + ", " + String.format("%.5f", lla[2]));
         ecef = lla2ecef(lla, a, e);
         Log.i("######", "ecef: " + String.format("%.5f", ecef[0]) + ", " + String.format("%.5f", ecef[1]) + ", " + String.format("%.5f", ecef[2]));
-        enu = ecef2enu(lla, ecef);
-        Log.i("########", "enu: " + String.format("%.5f", enu[0]) + ", " + String.format("%.5f", enu[1]) + ", " + String.format("%.5f", enu[2]));
     }
 
     public void setLla(double latitude, double longitude, double altitude) {
@@ -27,48 +25,28 @@ public class Coordinate {
         lla = new double[]{latitude, longitude, altitude};
     }
 
+    /**
+     * a Semi-major axis
+     * f Flattening
+     * e Eccentricity
+     */
     private double[] lla2ecef(double[] lla, double a, double e) {
-        double phi = Math.toRadians(lla[1]);
-        double lam = Math.toRadians(lla[0]);
+        double phi = Math.toRadians(lla[0]);
+        double lam = Math.toRadians(lla[1]);
         double h = lla[2];
 
         double e2 = Math.pow(e, 2);
         double sinPhi = Math.sin(phi);
-        // compute radius of curvature in prime vertical
-        double n = a / Math.sqrt(1 - e2 * Math.pow(sinPhi, 2));
+        double n = a / Math.sqrt(1 - e2 * Math.pow(sinPhi, 2));// radius of curvature in the prime vertical
         double distanceFromZ = (n + h) * Math.cos(phi);
 
         double x = distanceFromZ * Math.cos(lam);
         double y = distanceFromZ * Math.sin(lam);
         double z = (n * (1 - e2) + h) * sinPhi;
 
-        double[] ret = {x, y, z};
+        //match my coord sys
+        double[] ret = {-x, z, y};
         return ret;
-    }
-
-    private double[] ecef2enu(double[] lla, double[] ecef) {
-        double phi = Math.toRadians(lla[1]);
-        double lam = Math.toRadians(lla[0]);
-
-        double sinPhi = Math.sin(phi);
-        double cosPhi = Math.cos(phi);
-        double sinLam = Math.sin(lam);
-        double cosLam = Math.cos(lam);
-
-        double[] rotation = new double[]{
-                -sinLam, cosLam, 0,
-                (-sinPhi * cosLam), (-sinPhi * sinLam), cosPhi,
-                (cosPhi * cosLam), (cosPhi * sinLam), sinPhi
-        };
-
-        double enu[] = new double[3];
-        for (int i = 0; i < 3; i++) {
-            enu[i] = 0.0;
-            for (int j = 0; j < 3; j++) {
-                enu[i] += rotation[i * 3 + j] * ecef[j];
-            }
-        }
-        return enu;
     }
 
     @Override
--- app/src/main/res/raw/earth_fragment.shader
+++ app/src/main/res/raw/earth_fragment.shader
@@ -6,6 +6,6 @@ varying vec2 v_texCoord;
 
 void main()
 {
-    // vec2 flipped_texcoord = vec2(v_texCoord.x, 1.0 - v_texCoord.y);
-    gl_FragColor = texture2D(u_TexId, v_texCoord);
+    vec2 flipped_texcoord = vec2(1.0 - v_texCoord.x, v_texCoord.y);
+    gl_FragColor = texture2D(u_TexId, flipped_texcoord);
 }
