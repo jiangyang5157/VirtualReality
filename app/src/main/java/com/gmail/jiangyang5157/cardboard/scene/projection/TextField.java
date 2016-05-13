26b8e2f7a61e31687b3e6d8803a4e19757726856
Yang Jiang <jiangyang5157@gmail.com>
2016-05-13 15:04:58 +1200

    NA

--- app/src/main/java/com/gmail/jiangyang5157/cardboard/scene/projection/TextField.java
+++ app/src/main/java/com/gmail/jiangyang5157/cardboard/scene/projection/TextField.java
@@ -7,7 +7,6 @@ import android.opengl.GLES20;
 import android.opengl.GLUtils;
 import android.opengl.Matrix;
 import android.text.TextPaint;
-import android.util.Log;
 
 import com.gmail.jiangyang5157.cardboard.scene.Head;
 import com.gmail.jiangyang5157.tookit.app.AppUtils;
