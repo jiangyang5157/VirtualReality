9aac408867c18adf7a52b6763121dee3ff493ae3
Yang Jiang <jiangyang5157@gmail.com>
2016-06-04 13:58:27 +1200

    handle thread to create obj model

--- app/src/main/java/com/gmail/jiangyang5157/cardboard/vr/MainActivity.java
+++ app/src/main/java/com/gmail/jiangyang5157/cardboard/vr/MainActivity.java
@@ -2,10 +2,7 @@ package com.gmail.jiangyang5157.cardboard.vr;
 
 import android.opengl.GLES20;
 import android.opengl.Matrix;
-import android.os.AsyncTask;
 import android.os.Bundle;
-import android.os.Handler;
-import android.os.HandlerThread;
 import android.util.Log;
 import android.widget.Toast;
 
@@ -176,39 +173,22 @@ public class MainActivity extends GvrActivity implements GvrView.StereoRenderer 
 
             if (objModel != null) {
                 if (!objModel.isCreated()) {
-                    if (mHandlerThread == null){
-                        mHandlerThread = new HandlerThread("MainActivity");
-                        mHandlerThread.start();
-                        final Handler handler = new Handler(mHandlerThread.getLooper());
-                        handler.post(new MyRunable(1));
-                    } else if (objModel.isReadyToCreate()){
-                        objModel.create2();
+                    if (objModel.getCreationState() == ObjModel.STATE_BEFORE_PREPARE) {
+                        objModel.getHandler().post(new Runnable() {
+                            @Override
+                            public void run() {
+                                objModel.prepare();
+                            }
+                        });
+                    } else if (objModel.getCreationState() == ObjModel.STATE_BEFORE_CREATE) {
+                        objModel.create();
                         objModel.setPosition(head.getCamera().getPosition(), head.forward, head.quaternion);
-                        objModel.setReadyToCreate(false);
-                        mHandlerThread.quit();
-                        mHandlerThread = null;
                     }
                 }
             }
         }
 
         ray.setIntersection(getIntersection());
-    }
-
-    HandlerThread mHandlerThread;
-    class MyRunable implements Runnable {
-        int pos;
-        public MyRunable(int pos) {
-            this.pos = pos;
-        }
-        @Override
-        public void run() {
-            if (pos == 1) {
-                if (!objModel.isReadyToCreate()) {
-                    objModel.create1();
-                }
-            }
-        }
     }
 
     @Override
@@ -351,9 +331,6 @@ public class MainActivity extends GvrActivity implements GvrView.StereoRenderer 
     @Override
     protected void onDestroy() {
         super.onDestroy();
-        if (mHandlerThread != null) {
-            mHandlerThread.quit();
-        }
         if (objModel != null) {
             objModel.destroy();
         }
