ef70022801298279d90592c2443e2bb69550d6c7
Yang Jiang <jiangyang5157@gmail.com>
2016-07-03 23:21:45 +1200

    NA

--- app/src/main/res/raw/marker_color_vertex_shader.glsl
+++ app/src/main/res/raw/marker_color_vertex_shader.glsl
@@ -9,9 +9,9 @@ varying vec3 v_Normal;
 
 void main()
 {
-    const float reciprocalScaleOnScreen = 0.01;
-    float w = (u_MVPMatrix * vec4(0,0,0,1)).w * reciprocalScaleOnScreen;
-    vec4 pos = vec4(a_Position.xyz * w , 1);
+    //const float reciprocalScaleOnScreen = 0.01;
+    //float w = (u_MVPMatrix * vec4(0,0,0,1)).w * reciprocalScaleOnScreen;
+    //vec4 pos = vec4(a_Position.xyz * w , 1);
 
     // Transform the vertex into eye space.
     v_Position = vec3(u_MVMatrix * a_Position);
@@ -19,5 +19,5 @@ void main()
     // Transform the normal's orientation into eye space.
     v_Normal = vec3(u_MVMatrix * vec4(a_Normal, 0.0));
 
-    gl_Position = u_MVPMatrix * pos;
+    gl_Position = u_MVPMatrix * a_Position;
 }
