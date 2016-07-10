5954baa00c1c95de87a79f6e779afad0180ce65a
Yang Jiang <jiangyang5157@gmail.com>
2016-07-10 22:50:00 +1200

    octree.1

--- app/src/main/java/com/gmail/jiangyang5157/cardboard/scene/tree/SphereObj.java
+++ app/src/main/java/com/gmail/jiangyang5157/cardboard/scene/tree/SphereObj.java
@@ -6,7 +6,7 @@ import com.gmail.jiangyang5157.cardboard.scene.model.Sphere;
  * @author Yang
  * @since 7/10/2016
  */
-public class SphereObj extends TreeObject{
+public class SphereObj extends TreeObject {
 
     private Sphere sphere;
 
