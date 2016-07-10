2075047edbded1e04a1e188219f86b283cd70998
Yang Jiang <jiangyang5157@gmail.com>
2016-07-11 00:32:40 +1200

    octree.1

--- app/src/main/java/com/gmail/jiangyang5157/cardboard/scene/tree/TreeObject.java
+++ app/src/main/java/com/gmail/jiangyang5157/cardboard/scene/tree/TreeObject.java
@@ -9,4 +9,39 @@ public class TreeObject {
     protected float[] center;
 
     protected float radius;
+
+    public TreeObject(float[] center, float radius) {
+        this.center = center;
+        this.radius = radius;
+    }
+
+//    @Override
+//    public int hashCode() {
+//        int ret = 17;
+//        for (float data : center) {
+//            ret = 37 * ret + hashCode(data);
+//        }
+//        ret = 37 * ret + hashCode(radius);
+//        return ret;
+//    }
+//
+//    private int hashCode(double d) {
+//        long longBits = Double.doubleToLongBits(d);
+//        return (int) (longBits ^ longBits >>> 32);
+//    }
+//
+//    @Override
+//    public boolean equals(Object obj) {
+//        if (obj == null || this.getClass() != obj.getClass()) {
+//            return false;
+//        }
+//        if (this == obj) {
+//            return true;
+//        }
+//        TreeObject that = (TreeObject) obj;
+//        return this.center[0] == that.center[0]
+//                && this.center[1] == that.center[1]
+//                && this.center[2] == that.center[2]
+//                && this.radius == that.radius;
+//    }
 }
