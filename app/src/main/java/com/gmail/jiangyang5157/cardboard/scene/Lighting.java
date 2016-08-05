1631fa271648b597bc04807264bb31c22ebd8d89
Yang Jiang <jiangyang5157@gmail.com>
2016-08-05 17:51:19 +1200

    NA

--- app/src/main/java/com/gmail/jiangyang5157/cardboard/scene/Lighting.java
+++ app/src/main/java/com/gmail/jiangyang5157/cardboard/scene/Lighting.java
@@ -5,6 +5,7 @@ package com.gmail.jiangyang5157.cardboard.scene;
  * @since 4/30/2016
  */
 public interface Lighting {
+    // w component: 1 for points and 0 for vectors
     float[] LIGHT_POS_IN_CAMERA_SPACE_CENTER = new float[]{0.0f, 0.0f, 0.0f, 1.0f};
 
     float[] getLightPosInCameraSpace();
