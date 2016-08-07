4c9bbdd6178c60f09118dc92a4ae2d7a27e7d940
Yang Jiang <jiangyang5157@gmail.com>
2016-08-07 22:31:16 +1200

    NA

--- app/src/main/java/com/gmail/jiangyang5157/cardboard/scene/model/DescriptionField.java
+++ app/src/main/java/com/gmail/jiangyang5157/cardboard/scene/model/DescriptionField.java
@@ -3,6 +3,11 @@ package com.gmail.jiangyang5157.cardboard.scene.model;
 import android.content.Context;
 import android.graphics.Bitmap;
 
+import com.gmail.jiangyang5157.tookit.app.RegularExpressionUtils;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 /**
  * @author Yang
  * @since 8/6/2016
@@ -15,7 +20,18 @@ public class DescriptionField extends TextField {
 
     @Override
     protected Bitmap buildBitmap() {
+
+//        "^((https?|ftp)://|(www|ftp)\\\\.)?[a-z0-9-]+(\\\\.[a-z0-9-]+)+([/?].*)?$";
+//        Pattern pattern = Pattern.compile(RegularExpressionUtils.URL_TEMPLATE);
+//        Matcher match = pattern.matcher("example.com");//replace with string to compare
+//        if (match.find()) {
+//            System.out.println("String contains URL");
+//        }
+
+
+
         return super.buildBitmap();
     }
+
 
 }
